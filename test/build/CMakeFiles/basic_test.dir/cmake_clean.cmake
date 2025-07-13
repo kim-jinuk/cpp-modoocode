28aaf32752ef507f487ba0eae5d30143be4bb9fc
김진욱 <gjm8856@gmail.com>
2025-07-13 16:17:18 +0900

    notes update

---
+++ test/build/CMakeFiles/basic_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_test.dir/basic_test.o"
+  "CMakeFiles/basic_test.dir/basic_test.o.d"
+  "basic_test"
+  "basic_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
