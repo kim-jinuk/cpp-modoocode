b796bd573fc108b0355882f56f2651fb1acf7f9d
김진욱 <gjm8856@gmail.com>
2025-07-19 17:55:01 +0900

    add add_exception example

--- src/08_CPP_Exception/all_exception.cpp
+++ src/08_CPP_Exception/all_exception.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 #include <stdexcept>
 
-class parent : public std::exception {
-public:
-    virtual const char* what() const noexcept override { return "Parent!\n"; }
-};
-
-class child : public parent {
-public:
-    virtual const char* what() const noexcept override { return "Child!\n"; }
-};
-
 int func(int c) {
-    if (c == 1) throw parent();
-    else if (c == 2) throw child();
+    if (c == 1) {
+        throw 1;
+    } else if (c == 2) {
+        throw "hi";
+    } else if (c == 3) {
+        throw std::runtime_error("error");
+    }
+
     return 0;
 }
 
 int main() {
     int c;
     std::cin >> c;
-    
-    // 상속의 경우에 예외를 처리할 때는, 파생 클래스를 상위 catch에 먼저 작성한다.
+
     try {
         func(c);
-    } /*catch (parent& p) {
-        std::cout << "Parent Catch!" << std::endl;
-        std::cout << p.what();
-    } catch (child& c) {
-        std::cout << "Child Catch!" << std::endl;
-        std::cout << c.what();
-    }*/ catch (child& c) {
-        std::cout << "Child Catch!" << std::endl;
-        std::cout << c.what();
-    } catch (parent& p) {
-        std::cout << "Parent Catch!" << std::endl;
-        std::cout << p.what();
+    } catch (int e) {
+        std::cout << "Catch int : " << e << std::endl;
+    } catch (...) {
+        std::cout << "Default Error!" << std::endl;
     }
-    
+
     return 0;
 }
