6d22c4c2e06e60e16c01d892aaab4cc6ccb748cc
김진욱 <gjm8856@gmail.com>
2025-07-19 18:04:34 +0900

    bug fix

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
-#include <stdexcept>
 
-int func(int c) {
-    if (c == 1) {
-        throw 1;
-    } else if (c == 2) {
-        throw "hi";
-    } else if (c == 3) {
-        throw std::runtime_error("error");
-    }
+int foo() noexcept {}
 
-    return 0;
-}
+int bar() noexcept { throw 1; }
 
 int main() {
-    int c;
-    std::cin >> c;
-
+    // noexcept 함수 bar()에서 예외가 발생하면 예외가 제대로 처리되지 않고 프로그램이 종료됨.
+    // 하지만 noexcept를 사용하는 이유는, 프로그래머가 컴파일러에게 어떤 함수가 절대로 예외를 발생시키지 않는다는 힌트를 줌으로써 추가적인 최적화를 수행하기 위함이다.
+    // 대표적인 예로, C++11 이후로 소멸자들은 기본적으로 noexcept 이다.
+    /*
     try {
-        func(c);
-    } catch (int e) {
-        std::cout << "Catch int : " << e << std::endl;
-    } catch (...) {
-        std::cout << "Default Error!" << std::endl;
+        bar();
+    } catch (int x) {
+        std::cout << "Error : " << x << std::endl;
     }
+    */
 
     return 0;
 }
