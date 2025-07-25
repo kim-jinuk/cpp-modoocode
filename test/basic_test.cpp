56adce30f7e1044751500b12d4f9f3d625a7dad9
김진욱 <gjm8856@gmail.com>
2025-07-25 15:55:47 +0900

    update member_functor(std::mem_fn) example

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -18,8 +18,9 @@ int main() {
     container.push_back(d);
 
     vector<int> size_vec(4);
-    std::function<size_t(const vector<int>&)> sz_func = &vector<int>::size;
-    std::transform(container.begin(), container.end(), size_vec.begin(), sz_func);
+    //std::function<size_t(const vector<int>&)> sz_func = &vector<int>::size;
+    //std::transform(container.begin(), container.end(), size_vec.begin(), sz_func);
+    std::transform(container.begin(), container.end(), size_vec.begin(), std::mem_fn(&vector<int>::size));
 
     for (auto it = size_vec.begin(); it != size_vec.end(); ++it) {
         std::cout << "벡터의 크기: " << *it << std::endl;
