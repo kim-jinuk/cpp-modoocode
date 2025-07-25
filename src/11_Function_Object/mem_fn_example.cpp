48fa98ae776073bdd5b1383e88ead4cf54ec0157
김진욱 <gjm8856@gmail.com>
2025-07-25 16:10:59 +0900

    add mem_fn -> lambda case

--- src/11_Function_Object/mem_fn_example.cpp
+++ src/11_Function_Object/mem_fn_example.cpp
@@ -18,9 +18,15 @@ int main() {
     container.push_back(d);
 
     vector<int> size_vec(4);
+    // 함수 객체를 std::function으로 만듦
     //std::function<size_t(const vector<int>&)> sz_func = &vector<int>::size;
     //std::transform(container.begin(), container.end(), size_vec.begin(), sz_func);
-    std::transform(container.begin(), container.end(), size_vec.begin(), std::mem_fn(&vector<int>::size));
+
+    // 함수 객체를 mem_fn 함수로 만듦
+    // std::transform(container.begin(), container.end(), size_vec.begin(), std::mem_fn(&vector<int>::size));
+
+    // mem_fn 대신 람다 함수로도 가능
+    std::transform(container.begin(), container.end(), size_vec.begin(), [](const auto& v){ return v.size(); });
 
     for (auto it = size_vec.begin(); it != size_vec.end(); ++it) {
         std::cout << "벡터의 크기: " << *it << std::endl;
