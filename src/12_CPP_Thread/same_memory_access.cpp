aee324802b57df4051f326db8bec7be443d2a115
김진욱 <gjm8856@gmail.com>
2025-07-26 17:19:49 +0900

    add race condition example

--- src/12_CPP_Thread/same_memory_access.cpp
+++ src/12_CPP_Thread/same_memory_access.cpp
@@ -7,7 +7,7 @@ using std::thread;
 
 void worker(int& counter) {
     for (int i = 0; i < 10000; ++i) {
-        ++counter;
+        counter += 1;
     }
 }
 
@@ -17,6 +17,7 @@ int main() {
     vector<thread> workers;
     for (int i = 0; i < 4; ++i) {
         // 레퍼런스로 전달하려면 ref함수로 감싸야 한다. (지난 강좌의 bind 함수 참조)
+        // counter를 여러 쓰레드가 접근해서 수정하기 때문에 race condition이 발생한다.
         workers.push_back(thread(worker, std::ref(counter)));
     }
 
