d5e57bbe5637d9f22984f345ab72707eb9518756
김진욱 <gjm8856@gmail.com>
2025-08-01 18:51:50 +0900

    add ThreadPool_example2.cpp

---
+++ src/12_CPP_Thread/ThreadPool_example2.cpp
@@ -0,0 +1,102 @@
+#include <chrono>
+#include <cstdio>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <thread>
+#include <functional>
+#include <future>
+#include <queue>
+
+namespace ThreadPool {
+class ThreadPool {
+private:
+    size_t num_threads_;
+    std::vector<std::thread> worker_threads_;
+    std::queue<std::function<void()>> jobs_;
+    std::condition_variable cv_job_q_;
+    std::mutex m_job_q_;
+    bool stop_all;
+
+    void WorkerThread();
+    
+public:
+    ThreadPool(size_t num_threads);
+    ~ThreadPool();
+
+    template <class F, class... Args>
+    std::future<typename std::result_of<F(Args...)>::type> EnqueueJob(F f, Args... args);
+};
+
+ThreadPool::ThreadPool(size_t num_threads) : num_threads_(num_threads), stop_all(false) {
+    worker_threads_.reserve(num_threads_);
+    for (size_t i = 0; i < num_threads_; ++i) {
+        worker_threads_.emplace_back([this](){ this->WorkerThread(); });
+    }
+}
+
+void ThreadPool::WorkerThread() {
+    while (true) {
+        std::unique_lock<std::mutex> lock(m_job_q_);
+        cv_job_q_.wait(lock, [this]() { return !this->jobs_.empty() || stop_all; });
+
+        if (stop_all && jobs_.empty()) {
+            return;
+        }
+        
+        std::function<void()> job = std::move(jobs_.front());
+        jobs_.pop();
+        lock.unlock();
+
+        job();
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    stop_all = true;
+    cv_job_q_.notify_all();
+    for (auto& t : worker_threads_) {
+        t.join();
+    }
+}
+
+template <class F, class... Args>
+std::future<typename std::result_of<F(Args...)>::type> ThreadPool::EnqueueJob(F f, Args... args) {
+    if (stop_all) {
+        throw std::runtime_error("ThreadPool 사용 중지됨");
+    }
+    using return_type = typename std::result_of<F(Args...)>::type;
+    std::packaged_task<return_type()> job(std::bind(f, args...));
+
+    std::future<return_type> job_result_future = job.get_future();
+    {
+        std::lock_guard<std::mutex> lock(m_job_q_);
+        jobs_.push([&job]() { job(); });
+    }
+    cv_job_q_.notify_one();
+
+    return job_result_future;
+}
+}   // namespace ThreadPool
+
+int work(int t, int id) {
+    printf("%d start \n", id);
+    std::this_thread::sleep_for(std::chrono::seconds(t));
+    printf("%d end after %ds\n", id, t);
+    return t + id;
+}
+
+int main() {
+    ThreadPool::ThreadPool pool(3);
+    std::vector<std::future<int>> futures;
+/*
+    for (int i = 0; i < 10; ++i) {
+        futures.emplace_back(pool.EnqueueJob(work, i % 3 + 1, i));
+    }
+    for (auto& f : futures) {
+        printf("result : %d \n", f.get());
+    }
+
+*/
+    return 0;
+}
