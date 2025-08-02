6848d54e6aaa7fb32cda8643914575e71038fd67
김진욱 <gjm8856@gmail.com>
2025-08-02 20:11:22 +0900

    add uniform_initialization example

---
+++ src/13_Uniform_Initialization/basic_example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class A {
+public:
+    A() { std::cout << "A의 생성자 호출!" << std::endl; }
+};
+
+class B { 
+public:
+    B(A a) { std::cout << "B의 생성자 호출!" << std::endl; }
+};
+
+int main() {
+    A a();      // 뭐가 출력될까?
+    B b(A());   // 뭐가 출력될까?
+    
+    A a{};      // 균일한 초기화 (Uniform Initialization)
+
+    return 0;
+}
