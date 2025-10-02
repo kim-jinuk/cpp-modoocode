9cf0a9eded8c6fa46bfc525a3917b87dd6cc70ff
김진욱 <gjm8856@gmail.com>
2025-10-02 16:02:19 +0900

    add is_class_example.cpp of Chapter 16

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <type_traits>
 
+namespace detail {
+template <typename T>
+char test(int T::*);
+
+struct two {
+    char c[2];
+};
+
+template <typename T>
+two test(...);
+}   // namespace detail
+
+template <typename T>
+struct is_class : std::integral_constant<bool, sizeof(detail::test<T>(0)) == 1 && !std::is_union<T>::value> {};
+
+template <typename T>
+void only_class(const T& t) {
+    static_assert(is_class<T>::value);
+    std::cout << "T is an class \n";
+}
+
 class A {
 public:
     int n;
@@ -14,6 +35,8 @@ int main() {
     A a(3);
     std::cout << "a.n : " << a.n << std::endl;
     std::cout << "a.*p_n : " << a.*p_n << std::endl;
+    
+    only_class(a);
 
     return 0;
 }
