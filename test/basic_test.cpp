d1c50a0400870e54f2386b950c21b7930a141fde
김진욱 <gjm8856@gmail.com>
2025-10-07 15:56:46 +0900

    add enable_if_examples

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
 #include <type_traits>
+#include <iterator> // iterator_traits
 
-namespace detail {
+// C++11용 void_t 대체
+template <typename...>
+using void_t = void;
+
+// 기본: iterator 아님
+template <typename T, typename = void>
+struct is_iterator : std::false_type {};
+
+// 특수화 : iterator_traits<T>::iterator_category 가 있으면 iterator로 간주
 template <typename T>
-char test(int T::*);
-
-struct two {
-    char c[2];
-};
-
-template <typename T>
-two test(...);
-}   // namespace detail
-
-template <typename T>
-struct is_class : std::integral_constant<bool, sizeof(detail::test<T>(0)) == 1 && !std::is_union<T>::value> {};
+struct is_iterator<T, void_t<typename std::iterator_traits<T>::iterator_category>> : std::true_type {};
 
 template <typename T>
-void only_class(const T& t) {
-    static_assert(is_class<T>::value);
-    std::cout << "T is an class \n";
-}
-
-class A {
+class vector {
 public:
-    int n;
+    vector(size_t num, int elements) {
+        std::cout << elements << "를" << num << "개 만들기 \n";
+    }
 
-    A(int n) : n(n) {}
+    template <typename Iterator, typename = typename std::enable_if<is_iterator<Iterator>::value>::type>
+    vector(Iterator start, Iterator end) {
+        std::cout << "반복자를 이용한 생성자 호출 \n";
+    }
 };
 
 int main() {
-    int A::*p_n = &A::n;
-
-    A a(3);
-    std::cout << "a.n : " << a.n << std::endl;
-    std::cout << "a.*p_n : " << a.*p_n << std::endl;
-    
-    only_class(a);
+    vector<int> v(10, 3);
 
     return 0;
 }
--- test/build/CMakeFiles/basic_test.dir/compiler_depend.make
+++ test/build/CMakeFiles/basic_test.dir/compiler_depend.make
@@ -180,7 +180,9 @@ CMakeFiles/basic_test.dir/basic_test.cpp.o: ../basic_test.cpp \
   /usr/include/c++/11/bits/basic_ios.tcc \
   /usr/include/c++/11/bits/ostream.tcc \
   /usr/include/c++/11/istream \
-  /usr/include/c++/11/bits/istream.tcc
+  /usr/include/c++/11/bits/istream.tcc \
+  /usr/include/c++/11/iterator \
+  /usr/include/c++/11/bits/stream_iterator.h
 
 
 /usr/include/c++/11/bits/istream.tcc:
@@ -255,6 +257,8 @@ CMakeFiles/basic_test.dir/basic_test.cpp.o: ../basic_test.cpp \
 
 /usr/include/c++/11/bits/stl_iterator_base_funcs.h:
 
+/usr/include/c++/11/bits/stream_iterator.h:
+
 /usr/include/ctype.h:
 
 /usr/include/x86_64-linux-gnu/c++/11/bits/c++locale.h:
@@ -328,6 +332,8 @@ CMakeFiles/basic_test.dir/basic_test.cpp.o: ../basic_test.cpp \
 /usr/include/x86_64-linux-gnu/asm/errno.h:
 
 /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h:
+
+/usr/include/c++/11/iterator:
 
 /usr/include/c++/11/ostream:
 
