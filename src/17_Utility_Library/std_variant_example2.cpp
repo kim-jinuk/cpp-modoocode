013d465e32c9c07eea33ab08337120b078af7e2b
김진욱 <gjm8856@gmail.com>
2025-10-10 09:34:18 +0900

    fix CI error of std::variant examples

--- src/17_Utility_Library/std_variant_example2.cpp
+++ src/17_Utility_Library/std_variant_example2.cpp
@@ -12,8 +12,13 @@ public:
     B(int n) {}
 };
 
+class C {};
+
 int main() {
-    std::variant<A, B> v;
+    // std::variant<A, B> v;   // 해당 variant 객체 v에 값을 주지 않을 때, 첫 번째 타입인 A의 디폴트 생성자가 없기 때문에 에러가 난다!
+    // std::variant<A, C> v;   // std::variant<A, B> 에서 에러가 나는 것과 같은 이유로 에러가 남!
+    std::variant<C, A> v;       // 그러나, 첫 번 째 타입이 디폴트 생성자가 있으면 괜찮음.
+    std::variant<std::monostate, A, B> v;       // 이를 고려하지 않으려면 std::monostate를 사용한다.
 
     return 0;
 }
